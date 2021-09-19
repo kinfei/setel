50fcce398298e51d1bf232c6a954fb9946cc5a5f
Lee Kin Fei <kinfei.lee@qumonintelligence.com>
2021-09-19 23:16:10 +0800

    Rollback settings

--- setel-order-service/src/app.module.ts
+++ setel-order-service/src/app.module.ts
@@ -8,7 +8,9 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     OrdersModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/dev'),
+    MongooseModule.forRoot(
+      'mongodb://seteladmin:secret@0.0.0.0:27017/dev?authSource=admin',
+    ),
     // 'mongodb://seteladmin:secret@db:28017/dev?authSource=admin&readPreference=primary',
   ],
   controllers: [AppController],
